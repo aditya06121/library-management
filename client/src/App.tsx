fe5b32f066ba84cd93b7083fbe854d6cacc0e851
Aditya raj <adityaraj7320@gmail.com>
2026-03-14 21:03:16 +0530

    ci&cd + frontend + backend + nginx

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import DiceRoller from "./components/DiceRoller";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <DiceRoller />
+    </div>
+  );
+}
